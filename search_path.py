e1fe0802662bf55beb9e8912e9ce957e3eefd674
tjumyk <tjumyk@gmail.com>
2015-08-26 21:05:34 +1000

    push first version of Search Path Service

---
+++ search_path.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""Search Path Service for ROS based on A* algorithm.
+
+This service provides A* algorithm to compute the shortest path between two points in the OccupancyGrid map.
+"""
+
+__author__ = 'kelvin'
+
+import math
+import heapq
+
+import rospy
+from nav_msgs.msg import OccupancyGrid
+from assignment1.msg import *
+from assignment1.srv import *
+
+occ_threshold = 60
+inf = float('inf')
+
+
+def euclidean_distance(x1, y1, x2, y2):
+    """Get the euclidean distance between point (x1, y1) and (x2, y2)."""
+    dx = (x1 - x2)
+    dy = (y1 - y2)
+    return math.sqrt(dx * dx + dy * dy)
+
+
+def map_distance(map_grid, start, end):
+    """Get the map distance between start point and end point.
+
+    If occupancy >= occ_threshold, it will return Infinity, otherwise return the euclidean distance.
+    @type map_grid: OccupancyGrid
+    @type start: (int, int)
+    @type end: (int, int)
+    """
+    w = map_grid.info.width
+    goal_occ = map_grid.data[end[1] * w + end[0]]
+    if goal_occ >= occ_threshold:
+        return inf
+    return euclidean_distance(start[0], start[1], end[0], end[1])
+
+
+def cost_estimate(start, end):
+    """Estimate the cost from start point to end point.
+
+    Here we just simply use euclidean distance.
+    @type start: (int, int)
+    @type end: (int, int)
+    """
+    return euclidean_distance(start[0], start[1], end[0], end[1])
+
+
+def reconstruct_path(came_from, current):
+    """Reconstruct the WayPoint path from the initial(start) point to the current point.
+
+    @type came_from: dict
+    @type current: (int, int)
+    """
+    total_path = [WayPoint(current[0], current[1])]
+    while current in came_from:
+        current = came_from[current]
+        total_path.append(WayPoint(current[0], current[1]))
+    return total_path
+
+
+def neighbour_points(map_grid, point):
+    """Get the neighbouring points of a given point based on the map info.
+
+    If a candidate point's occ is -1, it will be discarded.
+    @type map_grid: OccupancyGrid
+    @type point: (int, int)
+    """
+    point_x = point[0]
+    point_y = point[1]
+    w = map_grid.info.width
+    h = map_grid.info.height
+    neighbours = []
+    if point_x > 0:
+        # left
+        if map_grid.data[point_y * w + (point_x - 1)] != -1:
+            neighbours.append((point_x - 1, point_y))
+        if point_y > 0:
+            # left top
+            if map_grid.data[(point_y - 1) * w + (point_x - 1)] != -1:
+                neighbours.append((point_x - 1, point_y - 1))
+        if point_y < w - 1:
+            # left bottom
+            if map_grid.data[(point_y + 1) * w + (point_x - 1)] != -1:
+                neighbours.append((point_x - 1, point_y + 1))
+    if point_x < w - 1:
+        # right
+        if map_grid.data[point_y * w + (point_x + 1)] != -1:
+            neighbours.append((point_x + 1, point_y))
+        if point_y > 0:
+            # right top
+            if map_grid.data[(point_y - 1) * w + (point_x + 1)] != -1:
+                neighbours.append((point_x + 1, point_y - 1))
+        if point_y < w - 1:
+            # right bottom
+            if map_grid.data[(point_y + 1) * w + (point_x + 1)] != -1:
+                neighbours.append((point_x + 1, point_y + 1))
+    # top
+    if point_y > 0 and map_grid.data[(point_y - 1) * w + point_x] != -1:
+        neighbours.append((point_x, point_y - 1))
+    # bottom
+    if point_y < h - 1 and map_grid.data[(point_y + 1) * w + point_x] != -1:
+        neighbours.append((point_x, point_y + 1))
+    return neighbours
+
+
+def a_star_search(map_grid, start, goal, **kwargs):
+    """Use A* algorithm to compute the shortest path from the start point to the goal point in the given map.
+    @type map_grid: OccupancyGrid
+    @type start: WayPoint
+    @type goal: WayPoint
+    """
+    closed_set = kwargs['closed_set'] if 'closed_set' in kwargs else set()
+    open_set = kwargs['open_set'] if 'open_set' in kwargs else set()
+    came_from = kwargs['came_from'] if 'came_from' in kwargs else dict()
+    g_score = kwargs['g_score'] if 'g_score' in kwargs else dict()
+    f_score = kwargs['f_score'] if 'f_score' in kwargs else dict()
+    open_heap = []  # priority queue for fast retrieval of the open point with the smallest f_score
+
+    # convert back to simple sequence
+    start_seq = (start.x, start.y)
+    goal_seq = (goal.x, goal.y)
+    g_score[start_seq] = 0
+    f_score[start_seq] = cost_estimate(start_seq, goal_seq)
+    open_set.add(start_seq)
+    heapq.heappush(open_heap, (f_score[start_seq], start_seq))
+
+    # when open set is not empty
+    while len(open_heap) > 0:
+        # get open point with smallest f_score
+        current_f_score, current_point = heapq.heappop(open_heap)
+        open_set.remove(current_point)
+        # if already reach the goal
+        if current_point == goal_seq:
+            return reconstruct_path(came_from, goal_seq)
+        # add current point into closed set (do not compute for it any more)
+        closed_set.add(current_point)
+        # get all valid neighbouring points of current point
+        for neighbour_point in neighbour_points(map_grid, current_point):
+            # if already closed
+            if neighbour_point in closed_set:
+                continue
+            # compute tentative g_score
+            tentative_g_score = g_score.get(current_point, inf) + map_distance(map_grid, current_point, neighbour_point)
+            # if already open
+            in_open_set = neighbour_point in open_set
+            if not in_open_set or tentative_g_score < g_score.get(neighbour_point, inf):
+                # update came_from and scores
+                came_from[neighbour_point] = current_point
+                g_score[neighbour_point] = tentative_g_score
+                f_score[neighbour_point] = tentative_g_score + cost_estimate(neighbour_point, goal_seq)
+                if not in_open_set:
+                    heapq.heappush(open_heap, (f_score[neighbour_point], neighbour_point))
+                    open_set.add(neighbour_point)
+    return []
+
+
+def handle_search_path(request):
+    """Handler function for a single service call.
+    @type request: SearchPathRequest
+    """
+    map_info = request.map.info
+    rospy.loginfo('Searching shortest path from (%d, %d) to (%d, %d) on a %d * %d map...' %
+                  (request.start.x, request.start.y, request.goal.x, request.goal.y,
+                   map_info.width, map_info.height))
+    path = a_star_search(request.map, request.start, request.goal)
+    if len(path) <= 0:
+        rospy.logwarn('Search path failed')
+    else:
+        rospy.loginfo('Found shortest path, length=%d' % len(path))
+        for i in range(len(path)):
+            point = path[i]
+            print '(%d, %d)->' % (point.x, point.y)
+    return SearchPathResponse(path)
+
+
+def search_path_server():
+    """Initialize "search_path_server" node and register "search_path" service."""
+    rospy.init_node('search_path_server')
+    rospy.Service('search_path', SearchPath, handle_search_path)
+    rospy.loginfo('Search path server is ready.')
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    search_path_server()
---
+++ search_path_tester.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""An interactive testing tool for validating A* algorithm in Search Path Service.
+
+This script read in given map file, and show the map in GUI. The user can 'right-click' any reasonable point to specify
+it as the starting point or the goal point (in this order), and then it will call A* algorithm to compute the shortest
+path, which will be rendered using blue arrows in the map. The start/end point will be filled with purple.
+
+The user can also 'left-click' to switch to different map modes:
+    1. Original map: show the occupancy grid using red-based colors. At each block, the bigger its occ value is, the
+        darker the color will be (occ=0: white, occ=100: black). One exception is the invalid block, which will be
+        filled with green color.
+    2. Closed-set map: show the final state of the closed set from the A* algorithm on top of the original map. Each
+        block within the closed set will be filled with light blue.
+    3. Open-set map: show the final state of the open set from the A* algorithm on top of the original map. Each block
+        within the open set will be filled with orange.
+"""
+
+__author__ = 'kelvin'
+
+import sys
+import time
+from colorsys import hls_to_rgb
+import Tkinter
+
+from assignment1.msg import WayPoint
+from nav_msgs.msg import OccupancyGrid
+
+from search_path import a_star_search
+
+unit = 20  # pixel per block in the map
+map_mode = 0  # map mode: [0]normal [1]closed set [2]open set
+canvas = None  # Tkinter canvas for rendering the map
+start_point = None  # start point of the requested path
+goal_point = None  # goal point of the requested path
+grid = None  # the occupancy grid (original map data)
+closed_set = set()  # closed set of A* algorithm
+open_set = set()  # open set of A* algorithm
+came_from = {}  # "came from" dict of A* algorithm
+path = []  # shorted path computed by A* algorithm
+
+
+def start():
+    """Start testing tool"""
+    global unit, start_point, goal_point, grid, closed_set, open_set, came_from, path, canvas
+    # handle param
+    if len(sys.argv) < 2:
+        print 'Give me map file'
+        exit()
+    input_map = sys.argv[1]
+    if len(sys.argv) > 2:
+        unit = int(sys.argv[2])
+    read_map(input_map)
+
+    # init map
+    top = Tkinter.Tk()
+    canvas = Tkinter.Canvas(top, bg="green", height=grid.info.height * unit, width=grid.info.width * unit)
+    draw_map()
+    canvas.bind("<Button-1>", change_mode)
+    canvas.bind("<Button-3>", change_points)
+    canvas.pack()
+    top.mainloop()
+
+
+# noinspection PyUnusedLocal
+def change_mode(event):
+    """change map mode"""
+    global map_mode
+    map_mode = (map_mode + 1) % 3
+    draw_map()
+
+
+def change_points(event):
+    """change start/goal point"""
+    global start_point, goal_point, path, closed_set, open_set, came_from
+    x = event.x / unit
+    y = event.y / unit
+    if start_point is None:
+        start_point = WayPoint(x, y)
+    elif goal_point is None:
+        goal_point = WayPoint(x, y)
+        call_a_star()
+    else:
+        path = []
+        closed_set = set()
+        open_set = set()
+        came_from = {}
+        start_point = WayPoint(x, y)
+        goal_point = None
+    draw_map()
+
+
+def call_a_star():
+    """call A* algorithm"""
+    global path, closed_set, open_set, came_from
+    time_start = time.clock()
+    path = a_star_search(grid, start_point, goal_point, closed_set=closed_set, open_set=open_set,
+                         came_from=came_from)
+    time_elapsed = time.clock() - time_start
+    if len(path) <= 0:
+        sys.stderr.write("Warning: A* returns no result!\n")
+    else:
+        print 'A* finished in %ss, path length = %d' % (str(time_elapsed), len(path))
+
+
+def read_map(map_file):
+    """Read map from given text file"""
+    global grid
+    grid = OccupancyGrid()
+    grid.info.resolution = 1
+    grid.data = list()
+    first_line = True
+    with open(map_file, 'r') as f:
+        for line in f:
+            if first_line:
+                param = [s for s in line.split(' ') if s]
+                grid.info.width = int(param[0])
+                grid.info.height = int(param[1])
+                first_line = False
+            else:
+                for num in line.split(' '):
+                    if num:
+                        grid.data.append(int(num))
+    assert len(grid.data) == grid.info.width * grid.info.height
+
+
+def draw_map():
+    """Render the map using canvas"""
+    canvas.delete('all')
+    width = grid.info.width
+    height = grid.info.height
+    for row in range(width):
+        for col in range(height):
+            val = grid.data[row * width + col]
+            color = 'green'
+            if val != -1:
+                rgb = hls_to_rgb(0, (100 - val) / 100.0, 1)
+                color = '#%0.2X%0.2X%0.2X' % (int(255 * rgb[0]), int(255 * rgb[1]), int(255 * rgb[2]))
+            canvas.create_rectangle(col * unit, row * unit, (col + 1) * unit, (row + 1) * unit, fill=color)
+    if map_mode == 1:
+        for point in closed_set:
+            canvas.create_rectangle(point[0] * unit, point[1] * unit, (point[0] + 1) * unit, (point[1] + 1) * unit,
+                                    fill='lightblue')
+    if map_mode == 2:
+        for point in open_set:
+            canvas.create_rectangle(point[0] * unit, point[1] * unit, (point[0] + 1) * unit, (point[1] + 1) * unit,
+                                    fill='orange')
+    if start_point is not None:
+        canvas.create_rectangle(start_point.x * unit, start_point.y * unit, (start_point.x + 1) * unit,
+                                (start_point.y + 1) * unit, fill='purple')
+    if goal_point is not None:
+        canvas.create_rectangle(goal_point.x * unit, goal_point.y * unit, (goal_point.x + 1) * unit,
+                                (goal_point.y + 1) * unit, fill='purple')
+    directions = [
+        ['↖', '↑', '↗'],
+        ['←', 'x', '→'],
+        ['↙', '↓', '↘']
+    ]
+    for point in [(p.x, p.y) for p in path]:
+        if point not in came_from:
+            continue
+        source_point = came_from[point]
+        x = point[0] - source_point[0] + 1
+        y = point[1] - source_point[1] + 1
+        canvas.create_text((source_point[0] + 0.5) * unit, (source_point[1] + 0.5) * unit, text=directions[y][x],
+                           font=("Ubuntu", unit),
+                           fill='blue')
+
+
+if __name__ == '__main__':
+    start()
