821c30dae805fea2f1a023c439474cb81a9038b7
Comp3431 15s2 - Adam Dobrzycki <z3470061@c3p0.cse.unsw.EDU.AU>
2015-09-01 10:51:18 +1000

    Updated explore.py (not tested)

--- explore.py
+++ explore.py
@@ -1,62 +1,135 @@
-__author__ = 'adam'
+#!/usr/bin/env python
+
+ 
 
 import rospy
-import search_path
-from nav_msgs.msg import OccupancyGrid
+import tf
+import math
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Twist
 
-done = False
-beaconsFound = 0
+navi_pub = None
+mode = 1
+MinDist = 0.3
+FindWall = 1
+Align = 2
+FollowWall = 3
+Adjust = 4
 
-def callback(map):
-    global done
-    global beaconsFound
+# 0.5 angular = 14 seconds revolution
 
-    if done == False:
-        nextCell = findNextCell(map)
-        path = findPath(nextCell)
-        traverse()
+def callback(data):
+    global mode
 
-        if foundBeacon():
-            beaconsFound += 1
-            if beaconsFound == 4:
-                done = True
-
-
-def findNextCell(map):
-    # convert current position into xy
-
-    # make array of -1 positions and their xy positions
-
-    unexplored = []
-
-    for 
-
-    # find closest xy position
+    if mode == FindWall:
+        findWall(data)
+    elif mode == Align:
+        align(data)
+    elif mode == FollowWall:
+        followWall(data)
+    elif mode == Adjust:
+        adjust(data)
 
 
+def findWall(data):
+    global mode
 
-    return 0
+    # check if we're within range of wall
+    for i in range(len(data.ranges)):
+        if data.ranges[i] < MinDist:
+            mode = Align
+
+    # else go forward
+    if mode == FindWall:
+        moveCmd = Twist()
+        moveCmd.linear.x = 0.1
+        moveCmd.angular.z = 0
+        navi_pub.publish(moveCmd)
 
 
-def findPath(nextCell):
-    rospy.wait_for_service('search_path')
-    try:
-        return SearchPath()
-    except rospy.ServiceException, e:
-        print "Service call failed"
+def align(data):
+    global mode
+
+    mid = len(data.ranges) / 2
+    offset = 256
+    thetaInd = mid + offset
+    threshold = 0.1
+
+    # if we are aligned then follow the wall, else rotate
+    if (data.ranges[thetaInd] < MinDist + threshold and
+        data.ranges[thetaInd] > MinDist - threshold):
+        mode = FollowWall
+    else:
+        moveCmd = Twist()
+        moveCmd.linear.x = 0
+        moveCmd.angular.z = 0.5
+        navi_pub.publish(moveCmd)
 
 
-def foundBeacon():
-    # call beaconRecognition service
-    return False
+def followWall(data):
+    global mode
 
-def traverse():
-    return False
+    mid = len(data.ranges) / 2
+    offset = 256
+    thetaInd = mid + offset
+
+    # if within wall range then keep moving forward
+    # else adjust
+    if (data.ranges[thetaInd] < MinDist + threshold and
+        data.ranges[thetaInd] > MinDist - threshold):
+        moveCmd = Twist()
+        moveCmd.linear.x = 0.1
+        moveCmd.angular.z = 0
+        navi_pub.publish(moveCmd)
+    else:
+        mode = Adjust
+
+
+def adjust(data):
+    global mode
+
+    mid = len(data.ranges) / 2
+    offset = 256
+    thetaInd = mid + offset
+
+    # if not within range then turn robot until in range
+    if (data.ranges[thetaInd] < MinDist + threshold and
+        data.ranges[thetaInd] > MinDist - threshold):
+        moveCmd = Twist()
+        moveCmd.linear.x = 0
+        moveCmd.angular.z = 0.5
+        navi_pub.publish(moveCmd)
+    else:
+        mode = FollowWall
+
+
+# http://answers.ros.org/question/31815/getting-coordinates-of-turtlebot/
+def getCurPos():
+    # set up
+    pBase = PoseStamped()
+    pMap = PoseStamped()
+    listener = tf.TransformListener()
+
+    # get pose of robot transformed into frame of map
+    pBase.header.frame_id = "base_link"
+    pBase.pose.position.x = 0
+    pBase.pose.position.y = 0
+    pBase.pose.orientation = tf().createQuaternionMsgFromYaw(0)
+    current_transform = rospy().now()
+    listener.getLatestCommonTime(pBase.header.frame_id, "map", current_transform)
+    pBase.header.stamp = current_transform
+    listener.transformPose("map", pBase, pMap)
+
+    return pMap
 
 
 def main():
+    global navi_pub
     rospy.init_node('explorer', anonymous=True)
-    rospy.Subscriber("/map", OccupancyGrid, callback)
+    navi_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+    rospy.Subscriber("/scan", LaserScan, callback)
+    r = rospy.Rate(10)
     rospy.spin()
 
 
